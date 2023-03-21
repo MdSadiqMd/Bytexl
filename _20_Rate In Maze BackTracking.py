449151206b5db748ccde2ca8ff1cd26202c89cee
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-03-21 10:44:16 +0530

    commmit

--- _20_Rate In Maze BackTracking.py
+++ _20_Rate In Maze BackTracking.py
@@ -29,7 +29,7 @@ def solvemazeutil(maze,x,y,sol):
         return False
 if __name__ == "__main__":
     maze=[ [1,0,0,0], 
-            [1,1,0,1], 
+        [1,1,0,1], 
             [0,1,0,0], 
             [1,1,1,1] ]
     solvemaze(maze)
